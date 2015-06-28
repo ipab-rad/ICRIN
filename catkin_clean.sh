7a131c51783ac2b9c2cc8d5445e5b36a7143db3e
GreatAlexander <alex.bordallo@gmail.com>
2015-06-28 16:48:54 +0100

    Create handy compilation scripts

---
+++ catkin_clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf ./../../devel
+rm -rf ./../../build
---
+++ catkin_make.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$#" -eq 1 ]; then
+  if [ "$1" == "msgs" ]; then
+    packages=($(ls ./icrin/icrin_msgs/))
+  else
+    packages=("$1")
+  fi
+else
+  packages=($(ls ./icrin/) $(ls ./icrin/icrin_msgs/))
+fi
+
+white_list=$(printf "%s;" "${packages[@]}")
+
+cd ./../..
+catkin_make -DCATKIN_WHITELIST_PACKAGES=$white_list
