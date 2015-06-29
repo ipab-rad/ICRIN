f33e7319f96a9d3232150044620b317a1603b6f0
GreatAlexander <alex.bordallo@gmail.com>
2015-06-29 18:10:00 +0100

    Create planner package core files, setup CMakeLists

---
+++ icrin/planner/include/planner/planner.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file      planner.hpp
+ * @brief     Add file description...
+ * @author    Alejandro Bordallo <alex.bordallo@ed.ac.uk>
+ * @date      2015-06-29
+ * @copyright (MIT) 2015 Edinferno
+ */
+
+#ifndef PLANNER_HPP
+#define PLANNER_HPP
+
+#include <ros/ros.h>
+
+class Planner {
+ public:
+  Planner();
+  ~Planner();
+
+ private:
+
+};
+
+#endif  /* PLANNER_HPP */
---
+++ icrin/planner/src/planner.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file      planner.cpp
+ * @brief     Planner master class, which instantiates different planners
+ * @author    Alejandro Bordallo <alex.bordallo@ed.ac.uk>
+ * @date      2015-06-29
+ * @copyright (MIT) 2015 Edinferno
+ */
+
+#include "planner/planner.hpp"
+
+Planner::Planner() {;}
+
+Planner::~Planner() {;}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "planner");
+  ros::NodeHandle nh("planner");
+
+  ros::spin();
+
+  ros::shutdown();
+
+  return 0;
+}
