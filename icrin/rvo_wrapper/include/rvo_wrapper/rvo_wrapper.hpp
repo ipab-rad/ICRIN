bda49e4c32c69832a2ebadc58a1e9c7e67a90eb2
GreatAlexander <alex.bordallo@gmail.com>
2015-06-30 18:05:20 +0100

    Implement proper rvo_wrapper class
    - Create Planner, do Planner step, add Planner Agent, get Agent pos are
    wrapper functions that receive ROS service calls and execute/return RVO
    methods/values.
    - Temporary assignment of Time Step and Prefered Velocity for testing
    purposes.
    - Commented out rvo_example, as there is no need to build it anymore.

--- icrin/rvo_wrapper/include/rvo_wrapper/rvo_wrapper.hpp
+++ icrin/rvo_wrapper/include/rvo_wrapper/rvo_wrapper.hpp
@@ -9,15 +9,45 @@
 #ifndef RVO_WRAPPER_HPP
 #define RVO_WRAPPER_HPP
 
+#include <ros/ros.h>
+
 #include <rvo_wrapper/RVO.h>
 #include <rvo_wrapper/Definitions.h>
+#include <rvo_wrapper/Vector2.h>
+
+#include <std_srvs/Empty.h>
+#include <rvo_wrapper_msgs/GetAgentPos.h>
+#include <rvo_wrapper_msgs/AddPlannerAgent.h>
 
 class RVOWrapper {
  public:
-  RVOWrapper();
+  RVOWrapper(ros::NodeHandle* nh);
   ~RVOWrapper();
 
+  void rosSetup();
+
+  bool createPlanner(std_srvs::Empty::Request& req,
+                     std_srvs::Empty::Response& res);
+
+  bool doPlannerStep(std_srvs::Empty::Request& req,
+                     std_srvs::Empty::Response& res);
+
+  bool addPlannerAgent(rvo_wrapper_msgs::AddPlannerAgent::Request& req,
+                       rvo_wrapper_msgs::AddPlannerAgent::Response& res);
+
+  bool getAgentPos(rvo_wrapper_msgs::GetAgentPos::Request& req,
+                   rvo_wrapper_msgs::GetAgentPos::Response& res);
  private:
+  // ROS
+  ros::NodeHandle* nh_;
+  ros::ServiceServer srv_create_planner_;
+  ros::ServiceServer srv_do_planner_step_;
+  ros::ServiceServer srv_add_planner_agent_;
+  ros::ServiceServer srv_get_agent_pos_;
+
+  // Class pointers
+  RVO::RVOSimulator* planner_;
+  std::vector<RVO::RVOSimulator*> sim_vect_;
 
 };
 
--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -8,6 +8,69 @@
 
 #include "rvo_wrapper/rvo_wrapper.hpp"
 
-RVOWrapper::RVOWrapper() {;}
+RVOWrapper::RVOWrapper(ros::NodeHandle* nh) {
+  nh_ = nh;
+  this->rosSetup();
+}
 
 RVOWrapper::~RVOWrapper() {;}
+
+void RVOWrapper::rosSetup() {
+  srv_create_planner_ = nh_->advertiseService("create_planner",
+                                              &RVOWrapper::createPlanner, this);
+  srv_do_planner_step_ = nh_->advertiseService("do_planner_step",
+                                               &RVOWrapper::doPlannerStep, this);
+  srv_add_planner_agent_ = nh_->advertiseService("add_planner_agent",
+                                                 &RVOWrapper::addPlannerAgent, this);
+  srv_get_agent_pos_ = nh_->advertiseService("get_agent_pos",
+                                             &RVOWrapper::getAgentPos, this);
+}
+// RVO::RVOSimulator* RVOWrapper::createSimulation() {;}
+
+bool RVOWrapper::createPlanner(std_srvs::Empty::Request& req,
+                               std_srvs::Empty::Response& res) {
+  planner_ = new RVO::RVOSimulator();
+  planner_->setTimeStep(0.1f);
+  return true;
+}
+
+bool RVOWrapper::doPlannerStep(std_srvs::Empty::Request& req,
+                               std_srvs::Empty::Response& res) {
+  planner_->doStep();
+  return true;
+}
+
+bool RVOWrapper::addPlannerAgent(rvo_wrapper_msgs::AddPlannerAgent::Request&
+                                 req,
+                                 rvo_wrapper_msgs::AddPlannerAgent::Response&
+                                 res) {
+  RVO::Vector2 agent_pos(req.agent_pos.x, req.agent_pos.y);
+  res.agent_id = planner_->addAgent(agent_pos, req.neighbor_dist,
+                                    req.max_neighbors, req.time_horizon,
+                                    req.time_horizon_obst, req.radius,
+                                    req.max_speed);
+  RVO::Vector2 temp_goal(1.0f, 1.0f);
+  planner_->setAgentPrefVelocity(res.agent_id, temp_goal);
+  return true;
+}
+
+bool RVOWrapper::getAgentPos(rvo_wrapper_msgs::GetAgentPos::Request& req,
+                             rvo_wrapper_msgs::GetAgentPos::Response& res) {
+  RVO::Vector2 agent_pos = planner_->getAgentPosition(req.agent_id);
+  res.agent_pos.x = agent_pos.x();
+  res.agent_pos.y = agent_pos.y();
+  return true;
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "rvo_wrapper");
+  ros::NodeHandle nh("rvo_wrapper");
+
+  RVOWrapper rvo_wrapper(&nh);
+
+  ros::spin();
+
+  ros::shutdown();
+
+  return 0;
+}
