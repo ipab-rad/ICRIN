5527017909d9c3477eaba55ffe1c301479eaa459
GreatAlexander <alex.bordallo@gmail.com>
2015-07-02 21:02:45 +0100

    Implement setAgentDefaults srv callback

--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -671,6 +671,32 @@ bool RVOWrapper::queryVisibility(
 bool RVOWrapper::setAgentDefaults(
   rvo_wrapper_msgs::SetAgentDefaults::Request& req,
   rvo_wrapper_msgs::SetAgentDefaults::Response& res) {
+  res.res = true;
+  if (req.sim_ids.size() == 0 && planner_init_) { // If Planner
+    planner_->setAgentDefaults(req.defaults.neighbor_dist,
+                               req.defaults.max_neighbors,
+                               req.defaults.time_horizon_agent,
+                               req.defaults.time_horizon_obst,
+                               req.defaults.radius,
+                               req.defaults.max_speed);
+  } else if (req.sim_ids.size() > 0) { // If Sim Vector
+    if (req.sim_ids.back() >= req.sim_ids.front()) { // If good sim id range
+      for (uint32_t i = req.sim_ids.front(); i < req.sim_ids.back(); ++i) {
+        sim_vect_[i]->setAgentDefaults(req.defaults.neighbor_dist,
+                                       req.defaults.max_neighbors,
+                                       req.defaults.time_horizon_agent,
+                                       req.defaults.time_horizon_obst,
+                                       req.defaults.radius,
+                                       req.defaults.max_speed);
+      }
+    } else {
+      ROS_WARN("Please provide a proper id range for sim_vector");
+      res.res = false;
+    }
+  } else {
+    ROS_WARN("RVO Planner not initialised!");
+    res.res = false;
+  }
   return true;
 }
 
