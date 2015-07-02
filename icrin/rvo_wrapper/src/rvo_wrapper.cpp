cf9865a344521026611212b898a214904438c2dd
GreatAlexander <alex.bordallo@gmail.com>
2015-07-02 20:57:25 +0100

    Implement addObstacle srv callback

--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -212,6 +212,26 @@ bool RVOWrapper::addAgent(
 bool RVOWrapper::addObstacle(
   rvo_wrapper_msgs::AddObstacle::Request& req,
   rvo_wrapper_msgs::AddObstacle::Response& res) {
+  res.res = true;
+  std::vector<RVO::Vector2> vertices; // Get msg obstacle vertices
+  for (uint32_t i = 0; i < req.vertices.size(); ++i) {
+    vertices.push_back(RVO::Vector2(req.vertices[i].x, req.vertices[i].y));
+  }
+  if (req.sim_ids.size() == 0 && planner_init_) { // If Planner
+    planner_->addObstacle(vertices);
+  } else if (req.sim_ids.size() > 0) { // If Sim Vector
+    if (req.sim_ids.back() >= req.sim_ids.front()) { // If good sim id range
+      for (uint32_t i = req.sim_ids.front(); i < req.sim_ids.back(); ++i) {
+        sim_vect_[i]->addObstacle(vertices);
+      }
+    } else {
+      ROS_WARN("Please provide a proper id range for sim_vector");
+      res.res = false;
+    }
+  } else {
+    ROS_WARN("RVO Planner not initialised!");
+    res.res = false;
+  }
   return true;
 }
 
