026b970fc00f32f767c726933a2b2fc09b77350d
GreatAlexander <alex.bordallo@gmail.com>
2015-07-02 21:04:03 +0100

    Implement setAgentRadius srv callback

--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -818,6 +818,22 @@ bool RVOWrapper::setAgentPrefVelocity(
 bool RVOWrapper::setAgentRadius(
   rvo_wrapper_msgs::SetAgentRadius::Request& req,
   rvo_wrapper_msgs::SetAgentRadius::Response& res) {
+  res.res = true;
+  if (req.sim_ids.size() == 0 && planner_init_) { // If Planner
+    planner_->setAgentRadius(req.agent_id, req.radius);
+  } else if (req.sim_ids.size() > 0) { // If Sim Vector
+    if (req.sim_ids.back() >= req.sim_ids.front()) { // If good sim id range
+      for (uint32_t i = req.sim_ids.front(); i < req.sim_ids.back(); ++i) {
+        sim_vect_[i]->setAgentRadius(req.agent_id, req.radius);
+      }
+    } else {
+      ROS_WARN("Please provide a proper id range for sim_vector");
+      res.res = false;
+    }
+  } else {
+    ROS_WARN("RVO Planner not initialised!");
+    res.res = false;
+  }
   return true;
 }
 
