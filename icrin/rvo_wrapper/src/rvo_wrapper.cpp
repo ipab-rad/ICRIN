8862580cad601e568040559f1da50933e0aa0a99
GreatAlexander <alex.bordallo@gmail.com>
2015-07-02 21:03:24 +0100

    Implement setAgentNeighborDist srv callback

--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -747,6 +747,22 @@ bool RVOWrapper::setAgentMaxSpeed(
 bool RVOWrapper::setAgentNeighborDist(
   rvo_wrapper_msgs::SetAgentNeighborDist::Request& req,
   rvo_wrapper_msgs::SetAgentNeighborDist::Response& res) {
+  res.res = true;
+  if (req.sim_ids.size() == 0 && planner_init_) { // If Planner
+    planner_->setAgentNeighborDist(req.agent_id, req.neighbor_dist);
+  } else if (req.sim_ids.size() > 0) { // If Sim Vector
+    if (req.sim_ids.back() >= req.sim_ids.front()) { // If good sim id range
+      for (uint32_t i = req.sim_ids.front(); i < req.sim_ids.back(); ++i) {
+        sim_vect_[i]->setAgentNeighborDist(req.agent_id, req.neighbor_dist);
+      }
+    } else {
+      ROS_WARN("Please provide a proper id range for sim_vector");
+      res.res = false;
+    }
+  } else {
+    ROS_WARN("RVO Planner not initialised!");
+    res.res = false;
+  }
   return true;
 }
 
