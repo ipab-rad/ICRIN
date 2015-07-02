588b9698d675f0901476afbdfd2828707920c3fe
GreatAlexander <alex.bordallo@gmail.com>
2015-07-02 20:59:57 +0100

    Implement getAgentObstacleNeighbor srv callback

--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -394,6 +394,24 @@ bool RVOWrapper::getAgentNumObstacleNeighbors(
 bool RVOWrapper::getAgentObstacleNeighbor(
   rvo_wrapper_msgs::GetAgentObstacleNeighbor::Request& req,
   rvo_wrapper_msgs::GetAgentObstacleNeighbor::Response& res) {
+  res.res = true;
+  if (req.sim_ids.size() == 0 && planner_init_) { // If Planner
+    res.obstacle_vertex = planner_->getAgentObstacleNeighbor(req.agent_id,
+                                                             req.agent_obstacle);
+  } else if (req.sim_ids.size() > 0) { // If Sim Vector
+    if (req.sim_ids.back() >= req.sim_ids.front()) { // If good sim id range
+      for (uint32_t i = req.sim_ids.front(); i < req.sim_ids.back(); ++i) {
+        res.obstacle_vertex = sim_vect_[i]->getAgentObstacleNeighbor(req.agent_id,
+                                                                     req.agent_obstacle);
+      }
+    } else {
+      ROS_WARN("Please provide a proper id range for sim_vector");
+      res.res = false;
+    }
+  } else {
+    ROS_WARN("RVO Planner not initialised!");
+    res.res = false;
+  }
   return true;
 }
 
