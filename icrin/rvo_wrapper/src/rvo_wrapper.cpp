0c790a498cec5f65b193238f8bb0380f04d5e90a
GreatAlexander <alex.bordallo@gmail.com>
2015-07-02 21:01:36 +0100

    Implement getGlobalTime srv callback

--- icrin/rvo_wrapper/src/rvo_wrapper.cpp
+++ icrin/rvo_wrapper/src/rvo_wrapper.cpp
@@ -559,6 +559,22 @@ bool RVOWrapper::getAgentVelocity(
 bool RVOWrapper::getGlobalTime(
   rvo_wrapper_msgs::GetGlobalTime::Request& req,
   rvo_wrapper_msgs::GetGlobalTime::Response& res) {
+  res.res = true;
+  if (req.sim_ids.size() == 0 && planner_init_) { // If Planner
+    res.global_time = planner_->getGlobalTime();
+  } else if (req.sim_ids.size() > 0) { // If Sim Vector
+    if (req.sim_ids.back() >= req.sim_ids.front()) { // If good sim id range
+      for (uint32_t i = req.sim_ids.front(); i < req.sim_ids.back(); ++i) {
+        res.global_time = sim_vect_[i]->getGlobalTime();
+      }
+    } else {
+      ROS_WARN("Please provide a proper id range for sim_vector");
+      res.res = false;
+    }
+  } else {
+    ROS_WARN("RVO Planner not initialised!");
+    res.res = false;
+  }
   return true;
 }
 
