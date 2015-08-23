a540e10e8b340dfe56c486f577a423cde8d3118a
GreatAlexander <alex.bordallo@gmail.com>
2015-08-23 21:13:16 +0100

    Implement first part of model framework
    - Implement Model Wrapper, which receives environment data and performs
    the inference computation.
    - Implement Sim Wrapper, which interfaces the Model Wrapper with the
    RVO  Wrapper.
    - ModelHypotheses msg and topic enables Environment to request Model to
    consider hypotheses counterfactually. They consist of: GoalSampling,
    GoalSequence and Awareness. Currently only GoalSequence is implemented.
    - The model loop consists of: inferGoals, setupModel, runSims.
    Currently only setupModel is implemented.
    - Setup Model creates the necessary simulations using EnvData,
    AgentDefaults and ModelHypotheses.
    - Robot_Model determines whether a planner agent exists (Data on
    different topic than EnvData)

---
+++ icrin/model/include/model/model_wrapper.hpp
@@ -0,0 +1,82 @@
+/**
+ * @file      model_wrapper.hpp
+ * @brief     Model Wrapper to manage modelling functions and sim wrapper
+ * @author    Alejandro Bordallo <alex.bordallo@ed.ac.uk>
+ * @date      2015-08-22
+ * @copyright (MIT) 2015 RAD-UoE Informatics
+ */
+
+#ifndef MODEL_WRAPPER_HPP
+#define MODEL_WRAPPER_HPP
+
+#include <ros/ros.h>
+
+#include <std_msgs/Bool.h>
+
+#include <model/sim_wrapper.hpp>
+
+#include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/Twist.h>
+#include <environment_msgs/EnvironmentData.h>
+#include <model_msgs/ModelHypotheses.h>
+#include <model_msgs/GoalHypothesis.h>
+#include <model_msgs/AwareHypothesis.h>
+
+class ModelWrapper {
+ public:
+  ModelWrapper(ros::NodeHandle* nh);
+  ~ModelWrapper();
+
+  void loadParams();
+  void init();
+  void rosSetup();
+
+  void robotPoseCB(const geometry_msgs::Pose2D::ConstPtr& msg);
+  void robotGoalCB(const geometry_msgs::Pose2D::ConstPtr& msg);
+  void robotVelCB(const geometry_msgs::Twist::ConstPtr& msg);
+  void envDataCB(const environment_msgs::EnvironmentData::ConstPtr& msg);
+  void modelCB(const model_msgs::ModelHypotheses::ConstPtr& msg);
+
+  void runModel();
+  void inferGoals();
+  void setupModel();
+  void runSims();
+
+ private:
+  // Flags
+  bool use_rvo_lib_;
+
+  // Constants
+  bool robot_model_;
+  float goal_sum_prior_;
+  float goal_history_discount_;
+  int goal_inference_history_;
+  int velocity_average_window_;
+  float prior_lambda_;
+
+  // Variables
+  std::string robot_name_;
+  std::string model_name_;
+  environment_msgs::EnvironmentData env_data_;
+  geometry_msgs::Pose2D robot_pose_;
+  geometry_msgs::Pose2D robot_goal_;
+  geometry_msgs::Twist robot_vel_;
+  model_msgs::ModelHypotheses hypotheses_;
+  std::vector<uint32_t> sampling_sims_;
+  std::vector<uint32_t> sequence_sims_;
+  std::vector<uint32_t> awareness_sims_;
+
+  // ROS
+  ros::NodeHandle* nh_;
+  ros::Subscriber robot_pose_sub_;
+  ros::Subscriber robot_goal_sub_;
+  ros::Subscriber robot_vel_sub_;
+  ros::Subscriber env_data_sub_;
+  ros::Subscriber model_hyp_sub_;
+
+  // Class pointers
+  SimWrapper* sim_wrapper_;
+};
+
+#endif  /* MODEL_WRAPPER_HPP */
